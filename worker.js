bf0a6b531d63bafe8019cc5169e1bc551c033012
CUONG DANG <achilles1902@gmail.com>
2026-04-06 13:16:43 +0700

    chore: remove wrangler Worker config, switching to Cloudflare Pages deployment

--- worker.js
+++
@@ -1,6 +0,0 @@
-// Cloudflare Worker entry point - serves Vite/React SPA with SPA routing support
-export default {
-  async fetch(request, env, ctx) {
-    return env.ASSETS.fetch(request)
-  },
-}
